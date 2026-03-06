048b1ce070a3724b26a834876b536af626574360
Sameer A. <114605553+saalUW@users.noreply.github.com>
2026-03-05 20:09:00 -0800

    Refactor/docs (#27)
    * init: update License, README, and pyproject
    
    * update: init
    
    * fix: correct badges
    
    * fix: correct License badge

--- src/limetr/__init__.py
+++ src/limetr/__init__.py
@@ -1,2 +1,37 @@
+"""
+LimeTr: Robust Linear Mixed Effects Models
+
+A Python package for solving mixed effects models with linear random effects
+and robust regression capabilities through trimming.
+"""
+
 from .core import LimeTr
 from .data import Data
+from .variable import FeVariable, ReVariable, Variable
+from .visual import funnel
+from .stats import (
+    Prior,
+    GaussianPrior,
+    UniformPrior,
+    LinearPrior,
+    LinearGaussianPrior,
+    LinearUniformPrior
+)
+from .linalg import SmoothMapping, LinearMapping
+
+__all__ = [
+    "LimeTr",
+    "Data", 
+    "FeVariable",
+    "ReVariable",
+    "Variable",
+    "funnel",
+    "Prior",
+    "GaussianPrior",
+    "UniformPrior", 
+    "LinearPrior",
+    "LinearGaussianPrior",
+    "LinearUniformPrior",
+    "SmoothMapping",
+    "LinearMapping"
+]
