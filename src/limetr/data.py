61a0cdb61c29259284a2b1dd1001cbb812296279
zhengp <zhengp@uw.edu>
2026-03-06 16:27:40 -0800

    fix the type-checker error for src/limetr/data.py

--- src/limetr/data.py
+++ src/limetr/data.py
@@ -6,53 +6,38 @@ Data module.
 """
 
 import operator
-from collections.abc import Iterable
-from numbers import Number
+from collections.abc import Sequence
 
 import numpy as np
+from numpy.typing import ArrayLike
 
-from limetr.utils import default_vec_factory, iterable
+from limetr.utils import default_vec_factory
 
 
-# pylint: disable=too-many-instance-attributes
 class Data:
     """
     Data containers observations and group information.
 
-    Attributes
+    Parameters
     ----------
-    obs : NDArray
+    obs
         Observations. Assumed to be sorted by the group id.
-    obs_se : NDArray
-        Standard deviations of observation. Default is one for each observation.
-    group_sizes : NDArray
-        Number of observations for each group. Default treat every observation
-        as a group by itself.
-    weight : NDArray
-        Weights for each observation. Default is one for each observation.
+    obs_se
+        Standard deviations of observation. Default is one.
+    group_sizes
+        Number of observations for each group. Default is ``None``.
+    weight
+        Weights for each observation. Default is one.
 
     """
 
     def __init__(
         self,
-        obs: Iterable,
-        obs_se: Number | Iterable = 1.0,
-        group_sizes: Iterable[int] | None = None,
-        weight: Number | Iterable = 1.0,
+        obs: ArrayLike,
+        obs_se: ArrayLike = 1.0,
+        group_sizes: Sequence[int] | None = None,
+        weight: ArrayLike = 1.0,
     ) -> None:
-        """
-        Parameters
-        ----------
-        obs
-            Observations. Assumed to be sorted by the group id.
-        obs_se
-            Standard deviations of observation. Default is one.
-        group_sizes
-            Number of observations for each group. Default is ``None``.
-        weight
-            Weights for each observation. Default is one.
-
-        """
         self.obs = obs
         self.obs_se = obs_se
         self.weight = weight
@@ -71,7 +56,7 @@ class Data:
     obs = property(operator.attrgetter("_obs"))
 
     @obs.setter
-    def obs(self, vec: Iterable) -> None:
+    def obs(self, vec: ArrayLike) -> None:
         vec = np.asarray(vec)
         if any(np.isnan(vec)):
             raise ValueError("`obs` must not containing nan(s).")
@@ -80,7 +65,7 @@ class Data:
     obs_se = property(operator.attrgetter("_obs_se"))
 
     @obs_se.setter
-    def obs_se(self, vec: Number | Iterable) -> None:
+    def obs_se(self, vec: ArrayLike) -> None:
         vec = default_vec_factory(vec, self.num_obs, vec_name="obs_se")
         if any(vec <= 0.0):
             raise ValueError("`obs_se` must be all positive.")
@@ -89,7 +74,7 @@ class Data:
     weight = property(operator.attrgetter("_weight"))
 
     @weight.setter
-    def weight(self, vec: Number | Iterable) -> None:
+    def weight(self, vec: ArrayLike) -> None:
         vec = default_vec_factory(vec, self.num_obs, vec_name="weight")
         if any(vec < 0) or any(vec > 1):
             raise ValueError("`weight` must be all between 0 and 1.")
@@ -98,18 +83,18 @@ class Data:
     group_sizes = property(operator.attrgetter("_group_sizes"))
 
     @group_sizes.setter
-    def group_sizes(self, vec: Iterable | None) -> None:
+    def group_sizes(self, vec: Sequence[int] | None) -> None:
         if vec is None:
-            vec = np.ones(self.num_obs, dtype=int)
-        elif iterable(vec):
-            vec = np.asarray(vec).astype(int)
+            group_sizes = np.ones(self.num_obs, dtype=int)
         else:
-            raise TypeError("Use `None` or a vector to set `group_sizes`.")
-        if any(vec <= 0.0):
+            group_sizes = np.asarray(vec, dtype=int)
+            if group_sizes.ndim != 1:
+                raise TypeError("Use `None` or a vector to set `group_sizes`.")
+        if (group_sizes <= 0.0).any():
             raise ValueError("`group_sizes` must be all positive.")
-        if sum(vec) != self.num_obs:
+        if group_sizes.sum() != self.num_obs:
             raise ValueError("Sum of `group_size` must equal to `num_obs`.")
-        self._group_sizes = vec
+        self._group_sizes = group_sizes
 
     def __repr__(self) -> str:
         return f"Data(num_obs={self.num_obs}, num_groups={self.num_groups})"
